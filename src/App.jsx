dd3c0f74527be7b20ac13822c36af033738b4585
TRINNO112 <kaushtubh457@gmail.com>
2026-08-09 15:16:48 +0530

    feat: restore natural page scrolling on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -32,28 +32,7 @@ function App() {
     };
   }, []);
 
-  useEffect(() => {
-    if (!isLoaded || gameState === 'PLAYING') {
-      document.body.style.overflow = 'hidden';
-      document.documentElement.style.overflow = 'hidden';
-      document.body.style.position = 'fixed';
-      document.body.style.width = '100%';
-      document.body.style.height = '100%';
-    } else {
-      document.body.style.overflow = '';
-      document.documentElement.style.overflow = '';
-      document.body.style.position = '';
-      document.body.style.width = '';
-      document.body.style.height = '';
-    }
-    return () => {
-      document.body.style.overflow = '';
-      document.documentElement.style.overflow = '';
-      document.body.style.position = '';
-      document.body.style.width = '';
-      document.body.style.height = '';
-    };
-  }, [isLoaded, gameState]);
+
 
   useEffect(() => {
     preloadAllImages((pct) => {
